42eabfd77eefa047c45637dcca2781a92c9c4ee3
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-05-20 14:45:15 -0700

    Add task

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Create a route to the home displaying `Hello HBNB`
+"""
+
+
+from flask import Flask
+
+
+# initialize the application using the variable `app`
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello():
+    '''Returns a simple greeting'''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', methods=['GET'], strict_slashes=False)
+def hbnb():
+    """returns a single line"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', methods=['GET'], strict_slashes=False)
+def c(text):
+    '''
+    Returns the text given in the url
+    '''
+    return "C " + text.replace("_", " ")
+
+@app.route('/python/', defaults={'text': 'is cool'},
+           methods=['GET'], strict_slashes=False)
+@app.route('/python/<text>', methods=['GET'], strict_slashes=False)
+def python(text):
+    '''
+    Returns a python route with a default value
+    if no text is passed
+    '''
+    return "Python " + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
